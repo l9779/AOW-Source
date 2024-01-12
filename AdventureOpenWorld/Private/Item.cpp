27125f8c20dd1df9442f5a39d95a1b6d488d7f72
Lucas B <lucas.b.dev@proton.me>
2024-01-11 22:09:26 -0300

    Wie die Welt sich dreht

--- AdventureOpenWorld/Private/Item.cpp
+++ AdventureOpenWorld/Private/Item.cpp
@@ -1,27 +1,30 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Item.h"
+#include "AdventureOpenWorld/AdventureOpenWorld.h"
 
-// Sets default values
 AItem::AItem()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
 }
 
-// Called when the game starts or when spawned
 void AItem::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+	FString Name = GetName();
+	FString Message = FString::Printf(TEXT("Object Name: %s"), *Name);
+
+	if (GEngine)
+		GEngine->AddOnScreenDebugMessage(0, 1.f, FColor::Cyan, Message);
+
+	DRAW_SPHERE(GetActorLocation())
 }
 
-// Called every frame
 void AItem::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	FString Message = FString::Printf(TEXT("Delta Time: %f"), DeltaTime);
+	UE_LOG(LogTemp, Warning, TEXT("%s"), *Message);
 
 }
 
