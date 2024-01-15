8632c3d4ed1ac2445118edfc2d2b6cfb571e8ba8
Lucas B <lucas.b.dev@proton.me>
2024-01-15 20:43:21 -0300

    EnhancedInput module added

--- AdventureOpenWorld/Private/Pawns/Bird.cpp
+++ AdventureOpenWorld/Private/Pawns/Bird.cpp
@@ -1,6 +1,7 @@
 #include "Pawns/Bird.h"
 #include "Components/CapsuleComponent.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "EnhancedInputSubsystems.h"
 
 ABird::ABird()
 {
@@ -21,11 +22,22 @@ void ABird::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer())) 
+		{
+			Subsystem->AddMappingContext(BirdMappingContext, 0);
+		}
+	}
 }
 
 void ABird::MoveForward(float AxisValue)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Value: %f"), AxisValue);
+}
+
+void ABird::Move(const FInputActionValue& value)
+{
 }
 
 void ABird::Tick(float DeltaTime)
--- AdventureOpenWorld/Public/Pawns/Bird.h
+++ AdventureOpenWorld/Public/Pawns/Bird.h
@@ -2,10 +2,13 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Pawn.h"
+#include "InputActionValue.h"
 #include "Bird.generated.h"
 
 class UCapsuleComponent;
 class USkeletalMeshComponent;
+class UInputMappingContext;
+class UInputAction;
 
 UCLASS()
 class ADVENTUREOPENWORLD_API ABird : public APawn
@@ -23,6 +26,14 @@ public:
 protected:
 	virtual void BeginPlay() override;
 	void MoveForward(float AxisValue);
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputMappingContext* BirdMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input)
+	UInputAction* MoveAction;
+
+	void Move(const FInputActionValue& value);
 private:
 	UPROPERTY(VisibleAnywhere)
 	UCapsuleComponent* Capsule;
