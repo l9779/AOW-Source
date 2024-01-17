37afffd4ccd369758f5bc1f2c32729fac7844cd8
Lucas B <lucas.b.dev@proton.me>
2024-01-17 17:24:29 -0300

    character camera control

---
+++ AdventureOpenWorld/Private/Characters/SlashCharacter.cpp
@@ -0,0 +1,73 @@
+#include "Characters/SlashCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
+
+ASlashCharacter::ASlashCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationRoll = false;
+	bUseControllerRotationYaw = false;
+
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetRootComponent());
+	SpringArm->TargetArmLength = 300.f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	ViewCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("ViewCamera"));
+	ViewCamera->SetupAttachment(SpringArm);
+}
+
+void ASlashCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (APlayerController* PlayerController = Cast<APlayerController>(GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			Subsystem->AddMappingContext(SlashMappingContext, 0);
+		}
+	}
+}
+
+void ASlashCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ASlashCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	if (UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(PlayerInputComponent))
+	{
+		EnhancedInputComponent->BindAction(MovementAction, ETriggerEvent::Triggered, this, &ASlashCharacter::Movement);
+	}
+}
+
+void ASlashCharacter::Movement(const FInputActionValue& Value)
+{
+	const FVector2D MovementDirection = Value.Get<FVector2D>();
+
+	if (GetController())
+	{
+		AddMovementInput(GetActorForwardVector(), MovementDirection.Y);
+		AddMovementInput(GetActorRightVector(), MovementDirection.X);
+	}
+}
+
+void ASlashCharacter::LookAround(const FInputActionValue& Value)
+{
+	const FVector2D LookDirectionAxis = Value.Get<FVector2D>();
+
+	if (GetController())
+	{
+		AddControllerPitchInput(LookDirectionAxis.Y);
+		AddControllerYawInput(LookDirectionAxis.X);
+	}
+}
---
+++ AdventureOpenWorld/Public/Characters/SlashCharacter.h
@@ -0,0 +1,44 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "InputActionValue.h"
+#include "SlashCharacter.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+class USpringArmComponent;
+class UCameraComponent;
+
+UCLASS()
+class ADVENTUREOPENWORLD_API ASlashCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	ASlashCharacter();
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputMappingContext* SlashMappingContext;
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* MovementAction;
+	UPROPERTY(EditAnywhere, Category = Input)
+	UInputAction* LookAroundAction;
+
+	void Movement(const FInputActionValue& Value);
+	void LookAround(const FInputActionValue& Value);
+private:	
+	UPROPERTY(VisibleAnywhere)
+	USpringArmComponent* SpringArm;
+
+	UPROPERTY(VisibleAnywhere)
+	UCameraComponent* ViewCamera;
+};
