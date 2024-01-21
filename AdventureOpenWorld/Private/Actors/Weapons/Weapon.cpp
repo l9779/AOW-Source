772c583ed0ef713bc59f21d5608983bcb86463e5
Lucas B <lucas.b.dev@proton.me>
2024-01-21 20:00:54 -0300

    weapon class added

---
+++ AdventureOpenWorld/Private/Actors/Weapons/Weapon.cpp
@@ -0,0 +1,11 @@
+#include "Actors/Weapons/Weapon.h"
+
+void AWeapon::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	Super::OnSphereOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex, bFromSweep, SweepResult);
+}
+
+void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
+}
